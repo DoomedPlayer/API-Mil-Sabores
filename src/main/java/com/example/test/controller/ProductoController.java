0724bba025f3c6659d0fb3c11b0d917ec7b125d7
DoomedPlayer <ni.beeger@duocuc.cl>
2025-11-17 18:26:35 -0300

    Ajuste seguridad 3

--- src/main/java/com/example/test/controller/ProductoController.java
+++ src/main/java/com/example/test/controller/ProductoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,6 +45,7 @@ public class ProductoController {
     @Autowired
     private ImageService imageService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     @Operation(summary = "Obtener todos los productos", description = "Obtiene una lista de todos los productos")
     public ResponseEntity<List<Producto>> obtenerProductos(){
