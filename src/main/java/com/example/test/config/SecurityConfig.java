c7f8531b97725233c99a70f1a066d3764516cc42
DoomedPlayer <ni.beeger@duocuc.cl>
2025-11-23 19:33:20 -0300

    Ajuste seguridad 9

--- src/main/java/com/example/test/config/SecurityConfig.java
+++ src/main/java/com/example/test/config/SecurityConfig.java
@@ -32,8 +32,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/producto/**").permitAll()
-                .requestMatchers("/api/producto/**").hasAuthority("Admin")
+                .requestMatchers("/api/producto/**").permitAll()
                 .requestMatchers("/api/usuario/**").hasAuthority("Admin")
                 .requestMatchers("/images/**").permitAll()
                 .anyRequest().authenticated()
